9a0285d5cd9f00ec29aedddb44a790304ccb8850
Daniele Guido <gui.daniele@gmail.com>
2017-11-28 17:13:52 +0100

    add memorial nodes

--- graph/index.js
+++ graph/index.js
@@ -28,20 +28,67 @@ let c = 0;
 async.waterfall([
   (next) => {
     c++;
-    console.log(_ye(c),_bb(`test neo4j connection`))
+    console.log(_ye(`\n${c}. `),_bb(`test neo4j connection`))
     let q = query.memo.get_nodes;
     // session.query(q)
     console.log(q)
     session.run(q, {
       limit: 10
     }).then(res => {
-      console.log(_gr(' v '), _bb('success.'), res.records.length);
+      console.log(_gr('    v '), _bb('success.'), res.records.length, _bb('records found.'));
       next(null);
     }).catch(next);
+  },
+
+  // get the list of the contents folder
+  (next) => {
+    c++;
+    console.log(_ye(`\n${c}. `),_bb(`get all glob nodes`))
+    
+    glob(settings.glob.path, {}, (err, files) => {
+      if(err)
+        next(err)
+      else{
+        console.log(_gr('    v '), _bb('success.'), files.length, _bb('files found.'));
+        next(null, files)
+      }
+    })
+  },
+  (files, next) => {
+    c++;
+    console.log(_ye(`\n${c}. `),_bb(`save nodes according to json files.`))
+    
+
+    let q = async.queue((filepath, callback) => {
+      let n = {
+        uid: path.basename(filepath, settings.glob.ext),
+        year: path.dirname(filepath).split('/').pop()
+      }
+      console.log(_bb('    '),filepath, _bb('->'), n.uid);
+      
+      let q = query.memo.merge;
+      session.run(q, n)
+        .then(res => {
+          let node = res.records[0]._fields[0];
+          console.log(_gr('    v '), _bb('created'), node.properties.uid, _bb('labelled as'), node.labels );
+          callback();
+          
+        })
+        .catch(err => {
+          q.kill()
+          next(err)
+        });
+      
+    })
+    q.push(files)
+    q.drain = next;
   }
 ], (err) => {
   if(err)
     console.log(err)
+  else
+    console.log(_bb(`---\nthat's all folks\n---`));
+        
   session.close();
   driver.close();
 })
--- graph/queries/memo.cyp
+++ graph/queries/memo.cyp
@@ -1,6 +1,11 @@
 // name: get_nodes
 //
 MATCH (mem:memorialc:memo)
-
 RETURN mem
 LIMIT {limit}
+
+// name: merge
+//
+MERGE (mem:memorialc:memo {uid:{uid}})
+SET mem.year = {year}
+RETURN mem
